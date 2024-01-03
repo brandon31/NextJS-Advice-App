a17526b9597b02f230ff0d5fe8edef21f3ebbf94
Brandon <106634008+brandon31@users.noreply.github.com>
2024-01-03 03:30:13 +0000

    Made the fetch request function

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,28 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [advice, setAdvice] = useState<string>("");
+  const [id, setId] = useState<number>(0);
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice, id } = response.data.slip;
+        setAdvice(advice);
+        setId(id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello</h1>
